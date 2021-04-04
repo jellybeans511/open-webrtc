707df8bc3a3ee165857e963ada03a2f671c19fa4
jellybeans511 <57423738+jellybeans511@users.noreply.github.com>
2021-04-04 21:11:57 +0900

    Add files via upload

--- docs/control.js
+++ docs/control.js
@@ -12,11 +12,6 @@ const Peer = window.Peer;
   const meta = document.getElementById('js-meta');
   const sdkSrc = document.querySelector('script[src*=skyway]');
 
-  meta.innerText = `
-    UA: ${navigator.userAgent}
-    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
-  `.trim();
-
   const localStream = await navigator.mediaDevices
     .getUserMedia({
       audio: true,
