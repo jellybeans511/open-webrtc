d1c5654b859cfb672b673f444034820581da628b
docteu <57423738+jellybeans511@users.noreply.github.com>
2023-02-01 16:05:01 +0900

    Update control.js

--- docs/control.js
+++ docs/control.js
@@ -121,7 +121,7 @@ const Peer = window.Peer;
   // Register callee handler
   peer.on('call', mediaConnection => {
     mediaConnection.answer(mediaStream, {
-      videoCodec: localVideoCodec
+      videoCodec: document.getElementById('video-codec').value
     });
     //videoCodec: String(document.getElementById('video-codec').value)
 
